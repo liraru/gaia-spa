d9697b04f8de5e8d07a8686c192fd388bd736e7b
Lorena Sineiro <losingar@gmail.com>
2023-12-05 21:49:03 +0100

    landing routing mmodule

--- src/app/modules/layout/landing/landing.module.ts
+++ src/app/modules/layout/landing/landing.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { LandingComponent } from './components/landing/landing.component';
+import { LandingRoutingModule } from './landing-routing.module';
 
 @NgModule({
-  declarations: [LandingComponent],
-  imports: [CommonModule]
+  imports: [CommonModule, LandingRoutingModule],
+  declarations: [LandingComponent]
 })
 export class LandingModule {}
