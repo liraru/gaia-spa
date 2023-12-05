670dfea82f681f596fd567fa0dbda71169da06b6
Lorena Sineiro <losingar@gmail.com>
2023-12-05 12:45:00 +0100

    empty landing module

--- src/app/modules/layout/navbar/navbar.module.ts
+++ src/app/modules/layout/navbar/navbar.module.ts
@@ -1,8 +1,6 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { NavbarComponent } from './components/navbar/navbar.component';
-
-
+import { NgModule } from '@angular/core';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
