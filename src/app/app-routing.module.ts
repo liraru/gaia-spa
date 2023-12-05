9c2f2a7eab80fc77d44357d2a6ef283e6017c320
Lorena Sineiro <losingar@gmail.com>
2023-12-05 13:30:10 +0100

    empty management module and routes config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { APP_ROUTES } from './constants/routes.constant';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: APP_ROUTES.DASHBOARD,
+    loadChildren: () => import(`./modules/layout/landing/landing.module`).then((m) => m.LandingModule)
+  },
+  {
+    path: APP_ROUTES.MANAGEMENT,
+    loadChildren: () => import(`./modules/sections/management/management.module`).then((m) => m.ManagementModule)
+  },
+  {
+    path: APP_ROUTES.CIBELES,
+    loadChildren: () => import(`./modules/sections/cibeles/cibeles.module`).then((m) => m.CibelesModule)
+  },
+  {
+    path: APP_ROUTES.MINERVA,
+    loadChildren: () => import(`./modules/sections/minerva/minerva.module`).then((m) => m.MinervaModule)
+  },
+  { path: '**', redirectTo: APP_ROUTES.DASHBOARD, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/modules/sections/management/management.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ManagementComponent } from './components/management/management.component';
+
+
+
+@NgModule({
+  declarations: [
+    ManagementComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ManagementModule { }
