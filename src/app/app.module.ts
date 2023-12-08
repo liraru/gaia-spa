a854744dedb645c91d53c08d06029efd5bd80832
Lorena Sineiro <losingar@gmail.com>
2023-12-08 21:59:24 +0100

    material installled, empty interceptor added to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
-import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS, HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LANG } from './constants/languages.constant';
 import { FooterModule } from './modules/layout/footer/footer.module';
 import { MenuModule } from './modules/layout/menu/menu.module';
 import { NavbarModule } from './modules/layout/navbar/navbar.module';
+import { InterceptorService } from './services/interceptor.service';
 import { NavigationStatusService } from './services/navigation-status.service';
-import { LANG } from './constants/languages.constant';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -21,6 +24,7 @@ export function HttpLoaderFactory(http: HttpClient) {
   declarations: [AppComponent],
   imports: [
     // * ANGULAR * //
+    CommonModule,
     BrowserModule,
     RouterModule,
     AppRoutingModule,
@@ -35,9 +39,15 @@ export function HttpLoaderFactory(http: HttpClient) {
     // * LAYOUT * //
     FooterModule,
     MenuModule,
-    NavbarModule
+    NavbarModule,
+    BrowserAnimationsModule
   ],
-  providers: [provideClientHydration(), NavigationStatusService],
+  providers: [
+    provideClientHydration(),
+    NavigationStatusService,
+    provideHttpClient(withFetch()),
+    { provide: HTTP_INTERCEPTORS, useClass: InterceptorService, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
